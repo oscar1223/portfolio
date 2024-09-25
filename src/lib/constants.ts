8d1aef77a491e0bca30b62961bbae107fcc0b1ee
oscar1223 <oscararauzp@gmail.com>
2024-09-25 17:26:49 +0100

    personalization

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/oscar1223",
+  linkedin: "https://www.linkedin.com/in/oscararauzperez/",
+  discord: "https://www.discordapp.com/users/642384629743222835",
+  x: "https://x.com/racsoscar_eth",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
